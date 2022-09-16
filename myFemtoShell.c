729e6679a48887b7181ee61cb104fdd9b325e7e1
ManarSalam <45368492+ManarSalam@users.noreply.github.com>
2022-09-16 08:55:01 +0200

    Add files via upload

---
+++ myFemtoShell.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+char text[150];
+
+printf("Sada S000t: test keda > ");
+while(1)
+{
+	fgets(text,sizeof(text),stdin);
+	if(strcmp(text,"exit\n")!=0)
+	{	
+	   printf("You said: %s",text);
+	   printf("Sada S000t: test keda > ");
+	}
+	else
+	{
+		printf("Good Bye :) \n");
+		break;
+	}
+}
+return 0;
+}
+
